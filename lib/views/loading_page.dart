5d96a322202787131cb6d66e528d9ce139aef352
RamiroCornidez <118946007+RamiroCornidez@users.noreply.github.com>
2024-04-09 20:11:33 -0700

    - added the assets directory (#1)
    - loading_page.dart
    	- updated the placeholder loading screen to be a Lottie json file.
     - landing_page.dart
    	- created the fact card to display rotating facts to be added later.
    	- created the upload card to display a randomized title pun.
    	- created the "How it works" pop up widget. The description will need to be added to this

--- lib/views/loading_page.dart
+++ lib/views/loading_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'landing_page.dart'; // Ensure this is created
+import 'landing_page.dart';
+import 'package:lottie/lottie.dart';
 
 class LoadingPage extends StatefulWidget {
   @override
@@ -13,7 +14,9 @@ class _LoadingPageState extends State<LoadingPage> {
     super.initState();
     Future.delayed(Duration(seconds: 3), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => LandingPage()));
+        context,
+        MaterialPageRoute(builder: (context) => LandingPage()),
+      );
     });
   }
 
@@ -21,13 +24,13 @@ class _LoadingPageState extends State<LoadingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlutterLogo(size: 100),
-            SizedBox(height: 20),
-            SpinKitRing(color: Colors.blue),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Lottie.asset('assets/loading.json'),
+            ],
+          ),
         ),
       ),
     );
