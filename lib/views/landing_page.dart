fad4b032bd22bbfc6a373cfe7a47cbe1e91e5bd4
Ramiro Cornidez <ramirocornidez@gmail.com>
2024-04-09 19:52:56 -0700

    - added the assets directory  - loading_page.dart 	- updated the placeholder loading screen to be a Lottie json file.  - landing_page.dart 	- created the fact card to display rotating facts to be added later. 	- created the upload card to display a randomized title pun. 	- created the "How it works" pop up widget. The description will need to be added to this

--- lib/views/landing_page.dart
+++ lib/views/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'dart:math';
 import 'results_page.dart';
 
 class LandingPage extends StatefulWidget {
@@ -9,6 +10,36 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   bool _isPickingFile = false; // Track if we're currently picking a file
+
+  List<String> environmentalFacts = [
+    "Fact 1",
+    "Fact 2",
+    "Fact 3",
+  ];
+
+  List<String> funnyPuns = [
+    "Pun 1",
+    "Pun 2",
+    "Pun 3",
+  ];
+
+  int environmentalFactIndex = 0;
+  int funnyPunIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start the periodic timer to rotate facts
+    Stream.periodic(Duration(seconds: 5)).listen((_) {
+      setState(() {
+        environmentalFactIndex =
+            (environmentalFactIndex + 1) % environmentalFacts.length;
+      });
+    });
+
+    // Select a random funny pun initially
+    funnyPunIndex = Random().nextInt(funnyPuns.length);
+  }
 
   void _pickFiles(BuildContext context) async {
     if (_isPickingFile) return; // Exit if file picker is already active
@@ -33,21 +64,92 @@ class _LandingPageState extends State<LandingPage> {
     });
   }
 
+  void _showHowItWorksDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("How it works"),
+          content: Text(
+              "Your explanation about how the application works goes here."),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Landing Page")),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
-            child: Center(child: Text("Environment Stats Here")),
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      environmentalFacts[environmentalFactIndex],
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          ElevatedButton(
-            onPressed: _isPickingFile
-                ? null
-                : () =>
-                    _pickFiles(context), // Disable button when picking a file
-            child: Text("Upload File"),
+          SizedBox(height: 20),
+          Expanded(
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      funnyPuns[funnyPunIndex],
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () {
+                        _showHowItWorksDialog(context);
+                      },
+                      child: Text(
+                        "How it works",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed:
+                          _isPickingFile ? null : () => _pickFiles(context),
+                      child: _isPickingFile
+                          ? CircularProgressIndicator()
+                          : Text("Upload File"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
